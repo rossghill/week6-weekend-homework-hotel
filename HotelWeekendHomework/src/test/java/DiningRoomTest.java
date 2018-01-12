f122f5070d51b2e9eda1b2df5c21dbdc02ea9a01
Ross Hill <rosshill88@gmail.com>
2018-01-12 14:28:40 +0000

    Added Dining Room tests

--- HotelWeekendHomework/src/test/java/DiningRoomTest.java
+++ HotelWeekendHomework/src/test/java/DiningRoomTest.java
@@ -1,2 +1,25 @@
+import RoomTypes.DiningRoom;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiningRoomTest {
+
+    DiningRoom diningroom;
+
+    @Before
+    public void before() {
+        diningroom = new DiningRoom(56, "James, Charles, Cuthbert");
+    }
+
+    @Test
+    public void diningRoomHasCapacity() {
+        assertEquals(56, diningroom.getCapacity());
+    }
+
+    @Test
+    public void diningRoomHasGuests() {
+        assertEquals("James, Charles, Cuthbert", diningroom.getGuests());
+    }
 }
